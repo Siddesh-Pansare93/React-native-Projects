eac5b292f18a35a94f1a37bbf2b712f86b333a00
Siddesh-Pansare93 <siddeshpansare93@gmail.com>
2024-12-16 11:13:19 +0530

    first commit

---
+++ firstapp/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import {
+  View , 
+  Text , 
+  SafeAreaView , 
+  Button
+} from 'react-native'
+
+export default function app(){
+  return(
+    <SafeAreaView>
+    <View>
+      <Text>
+        Hii ! This is Siddesh And this is first app and lets have fun together 
+      </Text>
+      <Button title="CLick me to ger started"/>
+    </View>
+  </SafeAreaView>
+  )
+}
+
