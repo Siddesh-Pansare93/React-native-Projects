43a0c9d62d23af4e7e2a243b8785980548f26ca5
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-12-17 16:05:49 +0530

    add Fancycard component

--- secondapp/App.tsx
+++ secondapp/App.tsx
@@ -2,6 +2,7 @@ import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
 import FlatCards from './components/FlatCards'
 import ElevatedCards from './components/ElevatedCards'
+import FancyCards from './components/FancyCards'
 
 const App = (): JSX.Element => {
   return (
@@ -11,6 +12,7 @@ const App = (): JSX.Element => {
           
           <FlatCards/>
           <ElevatedCards/>
+          <FancyCards/>
         </View>
       </ScrollView>
     </SafeAreaView>
