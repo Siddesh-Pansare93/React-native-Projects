8cf979d7d9ce9f37d133c6706573e6392c1febef
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-12-18 18:56:51 +0530

    add action card component

--- secondapp/App.tsx
+++ secondapp/App.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import FlatCards from './components/FlatCards'
 import ElevatedCards from './components/ElevatedCards'
 import FancyCards from './components/FancyCards'
+import ActionCard from './components/ActionCard'
 
 const App = (): JSX.Element => {
   return (
@@ -13,6 +14,7 @@ const App = (): JSX.Element => {
           <FlatCards/>
           <ElevatedCards/>
           <FancyCards/>
+          <ActionCard/>
         </View>
       </ScrollView>
     </SafeAreaView>
