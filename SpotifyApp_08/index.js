1dbc3b06ff0c2586c8d90bd1a1d60cec1965892d
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-01-18 20:45:00 +0530

    fix: correct playback service registration in index.js

--- SpotifyApp_08/index.js
+++ SpotifyApp_08/index.js
@@ -1,12 +1,11 @@
 /**
  * @format
  */
-
+import TrackPlayer from 'react-native-track-player';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
-import TrackPlayer from 'react-native-track-player';
-import { PlayBackService } from './musicService';
+import {playbackService} from './musicService'
 
 AppRegistry.registerComponent(appName, () => App);
-TrackPlayer.registerPlaybackService(()=> PlayBackService() )
+TrackPlayer.registerPlaybackService(() => playbackService);
